dc03f81b6e0bc54fd7b7e91d636deb09df73bad2
ZYuC <71123702+ZYuC-2002@users.noreply.github.com>
2023-12-22 16:47:21 +0800

    上架首頁海報 完 (剩下架首頁海報&cookies)

--- src/uploadposter.js
+++ src/uploadposter.js
@@ -12,9 +12,64 @@ export const UploadPoster = () => {
         console.log(cookies)
     },[search])
 
+    const [image, setImage] = useState('');
+    const [imageSrc, setImageSrc] = useState('');
+    
+    const updateImage = (e) => {
+        e.preventDefault();
+        setImage(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        displayImage();
+    };
+
+    const displayImage = async () => {
+        try {
+          // 使用fetch獲取img data
+          const response = await fetch(image);
+
+          if (!response.ok) {
+            throw new Error('無法獲取圖片');
+          }
+    
+          // 獲取img data的blob
+          const blob = await response.blob();
+          // 創建臨時URL
+          const url = URL.createObjectURL(blob);
+    
+          // 設img URL
+          setImageSrc(url);
+        } catch (err) {
+          // error
+          console.error(err);
+          setImageSrc('');
+        }
+    };
+    
     return(
         <>
             <Navbaremployee setSearch={setSearch}/>
+            <form className="inputImgURL" onSubmit={(e) => handleSubmit(e)}>
+                <div>
+                    <div className="title">宣傳海報上傳</div>
+                    <input
+                        className="imgURL"
+                        id="image"
+                        type="text"
+                        value={image}
+                        onChange={updateImage}
+                    />
+                </div>
+                <button type="submit">上傳</button>
+            </form>
+
+            {imageSrc && (
+                <div className="previewImg">
+                    <img src={imageSrc} alt="input_image" />
+                </div>
+            )}
         </>
     );
 };
